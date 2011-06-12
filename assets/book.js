998d8ce6151e599267a66a56b901cbfb14316548
Fabrizio Giordano <giordano.fabrizio@gmail.com>
2011-06-12 16:37:28 +0200

    screen, window, document height base

--- assets/book.js
+++ assets/book.js
@@ -2,49 +2,63 @@ var bookScroll;
 //loadBook
 
 function scr () {
-  document.getElementById('book').style.height = "1000px";
+  document.getElementById('book').style.height = screen.width + 100 + "px";
   window.scrollTo(0,1);
 }
 
 function book (){
   var page = 0,
-      lineheight,
-      barheight = ( ("standalone" in window.navigator) && window.navigator.standalone ) ? 0 : 40;
-      
-  lineheight = document.defaultView.getComputedStyle(document.getElementById('book0'),null).getPropertyValue('line-height');
-  lineheight = Number(lineheight.replace(/px$/, ''));
-    
-  var spaceHeight = screen.height - document.getElementById('header').offsetHeight - document.getElementById('footer').offsetHeight - barheight;
-  var spaceHeightOriginal = spaceHeight;
-  spaceHeight = Math.floor( spaceHeight / lineheight ) * lineheight;
+      doc = document,
+      sH = screen.height,
+      sW = screen.width,
+      barHeight = ( ("standalone" in window.navigator) && window.navigator.standalone ) ? 0 : 40,
+      hH = doc.getElementById('header').offsetHeight,
+      fH = doc.getElementById('footer').offsetHeight,
+      book = doc.getElementById('book0'),
+      bookfooter = doc.getElementById('bookfooter'),
+      totpages = doc.getElementById('totpages'),
+      scroller = doc.getElementById('scroller'),
+      lineHeight = doc.defaultView.getComputedStyle(book,null).getPropertyValue('line-height').replace(/px$/, '') | 0,
+      bookHeight = book.offsetHeight, //scrollHeight
+      spaceHeight = Math.floor( (sH - hH - fH - barHeight) / lineHeight ) * lineHeight,
+      numPages = Math.ceil( bookHeight / spaceHeight ),
+      bookFooterHeight = spaceHeight * numPages - bookHeight + lineHeight,
+      debug = false;
   
-  var book = document.getElementById('book0');
-  //var bookHeight = book.scrollHeight;
-  var bookHeight = book.offsetHeight;
-  var num_pages = Math.ceil( bookHeight / spaceHeight );
-  var bookFooterHeight = spaceHeight * num_pages - bookHeight + lineheight;
-  
-  book.style.width = screen.width + "px";
-  
-  document.getElementById('bookfooter').style.height = bookFooterHeight + "px";
-  document.getElementById('book').style.height = screen.height + 50 + "px";
-  
-  document.getElementById('totpages').innerHTML = num_pages;
-  document.getElementById('scroller').style.width = screen.width * num_pages + "px";
-  document.getElementById('scroller').style.height = spaceHeight + "px";
+  book.style.width = sW + "px";
+  book.style.height = spaceHeight + "px";
+  bookfooter.style.height = bookFooterHeight + "px";
+  totpages.innerHTML = numPages;
+  scroller.style.height = spaceHeight + "px";
+  scroller.style.width = sW * numPages + "px";
   
   
-  for (i=1; i<num_pages; i++) {
+  for (i = 1; i < numPages; i++) {
     var clone = book.cloneNode(true);
     var page = book.parentNode.appendChild(clone);
     page.id = "book"+i;
     page.style.height = spaceHeight + "px";
-    page.style.width = screen.width + "px";
-    //page.style.left = screen.width*i + "px";
-    // page.style.top = -spaceHeight*i + "px";
+    page.style.width = sW + "px";
     page.scrollTop = i*spaceHeight;
-    //alert(i*spaceHeight);
   }
+  
+  /*
+  var clone = book.cloneNode(true),
+      frag = document.createDocumentFragment(),
+      el;
+
+  for (i=1; i<numPages; i++) {
+    page = clone;
+    page.id = "book"+i;
+    page.style.height = spaceHeight + "px";
+    page.style.width = sW + "px";
+    page.scrollTop = i*spaceHeight;
+    frag.appendChild(page); // better!
+  }
+
+  book.parentNode.appendChild(frag);
+  
+  */
   
   bookScroll = new iScroll('wrapper', {
     snap: true,
@@ -59,31 +73,31 @@ function book (){
     }
   });
   
-  log.error('lineheight: ' + lineheight);
-  
-  log.error('screen.height: ' + screen.height);
-  log.error('screen.width: ' + screen.width);
-  
-  log.error('document.height: ' + document.height);
-  log.error('document.width: ' + document.width);
-  
-  log.error('window.height: ' + window.height);
-  log.error('window.width: ' + window.width);
-  
-  log.error('header offsetHeight: '+ document.getElementById('header').offsetHeight);
-  log.error('footer offsetHeight: '+ document.getElementById('footer').offsetHeight);
-  log.error('num_pages: ' + num_pages);
-  log.error('spaceHeightOriginal: ' + spaceHeightOriginal);
-  log.error('spaceHeight: ' + spaceHeight);
-  log.error('bookHeight: ' + bookHeight);
-  log.error('bookHeight2: ' + book.offsetHeight);
-  log.error('spaceHeight: ' + spaceHeight);
-  log.error('scroller width: ' + document.getElementById('scroller').style.width);
-  log.error('scroller height: ' + document.getElementById('scroller').style.height);
+  if(debug) {
+    log.error('lineHeight: ' + lineHeight);
+
+    log.error('screen.height: ' + screen.height);
+    log.error('screen.width: ' + screen.width);
+
+    log.error('document.height: ' + document.height);
+    log.error('document.width: ' + document.width);
+
+    log.error('window.height: ' + window.height);
+    log.error('window.width: ' + window.width);
+
+    log.error('header offsetHeight: '+ document.getElementById('header').offsetHeight);
+    log.error('footer offsetHeight: '+ document.getElementById('footer').offsetHeight);
+    log.error('numPages: ' + numPages);
+    log.error('spaceHeight: ' + spaceHeight);
+    log.error('bookHeight: ' + bookHeight);
+    log.error('bookHeight2: ' + book.offsetHeight);
+    log.error('spaceHeight: ' + spaceHeight);
+    log.error('scroller width: ' + document.getElementById('scroller').style.width);
+    log.error('scroller height: ' + document.getElementById('scroller').style.height);
+  }
   
   // bookScroll.scrollToPage(5, 0, 500);
-  
-  setTimeout(scrollTo, 0, 0, 1);
+
 };
 
 window.onload = function(){
